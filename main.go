bf66e00592c9c17c4cddaf433776d77edb1a0802
Paulo Valentim <rickph_@hotmail.com>
2022-06-20 12:49:36 -0300

    initial commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import route2 "github.com/paulohenrique22/application/route"
+
+func main() {
+	route := route2.Route({
+		"ID":"1",
+		"ClientID":"1"
+	})
+
+	route.LoadPositions()
+	stringjson, _ := route.ExportJsonPositions()
+
+	fmt.Println(stringjson[0])
+}
